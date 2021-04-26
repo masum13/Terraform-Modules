4fb1ca9dfc7cca1aa3726add6a62d382aa250fdd
Masum Patel <62187971+masum13@users.noreply.github.com>
2021-04-26 12:15:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
-variable "region" {}
-variable "bucket_prefix" {}
-variable "bucket_name" {}
+variable "region" {
+  default = "us-east-1"  
+}
+variable "bucket_prefix" {
+  default = "jenkins"  
+}
+variable "bucket_name" {
+  default = "masum13199890"  
+}
